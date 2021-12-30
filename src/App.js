fd776480e4549a3e9e72c98b95d83f684fcb6b6f
diegomaustem <diego.maustem@gmail.com>
2021-12-30 17:17:42 -0300

    aula-20

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
-import Container from './components/layout/Container';
-import Company from './components/pages/Company';
-import Contact from './components/pages/Contact';
-import Home from './components/pages/Home';
-import NewProject from './components/pages/NewProject';
+
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Projects from'./components/pages/Projects'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+import Footer from './components/layout/Footer'
+import NavBar from'./components/layout/NavBar'
+
+
+
 
 function App() {
   return (
    <Router>
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/company">Contato</Link>
-            <Link to="/contact">Empresa</Link>
-            <Link to="/newproject">Novo Projeto</Link>
-        </div>
+     <NavBar />
         <Container customClass="min-height">
           <Routes>
                 <Route exact path="/" element={<Home />} > </Route>
+                <Route exact path="/projects" element={<Projects />} > </Route>
                 <Route exact path="/company" element={<Company />} > </Route>
                 <Route exact path="/contact" element={<Contact />} > </Route>
                 <Route exact path="/newproject" element={<NewProject />} > </Route>
           </Routes>
         </Container>
-        <p>Footer</p>
+      <Footer />
    </Router>
   );
 }
